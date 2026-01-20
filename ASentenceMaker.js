54bd77ef018cce95d1a4931f6650702e7bdc39b5
Janvi Singhal <janvisinghal10@gmail.com>
2026-01-20 20:07:53 +0530

    Build a Sentence Maker

---
+++ ASentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "beautiful";
+let noun = "lady";
+let verb = "walking";
+let place = "london";
+let adjective2 = "gorgeous";
+let noun2 = "apple";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "pretty";
+noun = "chair";
+verb = "eating";
+place = "japan";
+adjective2 = "handsome";
+noun2 = "table";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
