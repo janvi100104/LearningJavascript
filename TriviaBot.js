c6985d439d355cd91eaadbfa209cc00d5e1898be
Janvi Singhal <janvisinghal10@gmail.com>
2026-01-20 19:22:15 +0530

    Built a JavaScript Trivia Bot

---
+++ TriviaBot.js
@@ -0,0 +1,14 @@
+console.log("Hello! I'm your coding fun fact guide!" );
+let botName = "Chitti";
+let botLocation = "South";
+let favoriteLanguage = "JS";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact ="Fun part of learning is im teaching others while leaning " + favoriteLanguage +" myself.";
+console.log(codingFact);
+codingFact  = "Fun part of learning is im teaching others while leaning " + favoriteLanguage +" myself Its tough.";
+console.log(codingFact);
+codingFact  = "Even i am not getting " + favoriteLanguage +".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from "+ botLocation +".");
